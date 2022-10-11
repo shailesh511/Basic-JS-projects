8c822943bbb410d9f3fbc0e2a5faa2ff42a970ca
Shailesh <shaileshtripathi511@gmail.com>
2022-10-12 01:01:54 +0530

    attempt 1 to fix start and stop button

--- StopWatch app/index.js
+++ StopWatch app/index.js
@@ -10,62 +10,75 @@ let displaySeconds="";
 let displayMinutes="";
 let displayHours="";
 
-let watch= "stopped"
+let count= 0;
 
 start.addEventListener("click",()=>{
-    
-    start.innerText = "Pause"
+    count++;
+        window.setInterval(()=>{
+            if(count % 2 !== 0)
+            {
+                start.innerText = "Pause"
 
-    watch ="start"
- 
-    seconds++;
+                watch ="start"
+            
+                seconds++;
+            
+                //if one minutes passed then set seconds to 0 and increment minute to 1
+                if(seconds/60===1)
+                {
+                    seconds =0;
+                    minutes++;
+                    //if one hour passed then set minutes to 0 and increment hour to 1
+                    if(minutes/60===1)
+                    {
+                        minutes=0;
+                        hours++;
+                    }
+                }
+                
+                //if seconds variable is less then 0 then
+                if(seconds<10)
+                {
+                    displaySeconds = '0'+ seconds.toString();
+                }
+                else if(seconds>=10)
+                {
+                    displaySeconds =  seconds;
+                }
+
+                //if minutes variable is less then 0 then
+                if(minutes<10)
+                {
+                    displayMinutes = '0'+ minutes.toString();
+                }
+                else if(minutes>=10)
+                {
+                    displayMinutes =  minutes;
+                }
+
+                //if minutes variable is less then 0 then
+                if(hours<10)
+                {
+                        displayHours = '0'+ hours.toString();
+                }
+                else if(hours>=10)
+                {
+                    displayHours =  hours;
+                }
+
+                document.querySelector("#display").innerHTML = displayHours + ":" +displayMinutes +":"+ displaySeconds;
+            }
+         },1000)
    
-    //if one minutes passed then set seconds to 0 and increment minute to 1
-    if(seconds/60===1)
-    {
-        seconds =0;
-        minutes++;
-        //if one hour passed then set minutes to 0 and increment hour to 1
-        if(minutes/60===1)
-        {
-            minutes=0;
-            hours++;
-        }
-    }
-    
-    //if seconds variable is less then 0 then
-    if(seconds<10)
-    {
-         displaySeconds = '0'+ seconds.toString();
-    }
-    else if(seconds>=10)
-    {
-        displaySeconds =  seconds;
-    }
-
-    //if minutes variable is less then 0 then
-    if(minutes<10)
-    {
-        displayMinutes = '0'+ minutes.toString();
-    }
-    else if(minutes>=10)
-    {
-        displayMinutes =  minutes;
-    }
-
-    //if minutes variable is less then 0 then
-    if(hours<10)
-    {
-            displayHours = '0'+ hours.toString();
-    }
-    else if(hours>=10)
-    {
-        displayHours =  hours;
-    }
-
-   document.querySelector("#display").innerHTML = displayHours + ":" +displayMinutes +":"+ displaySeconds;
-
-})
+        if(count%2=== 0)
+       {
+        start.innerText = "Start"
+        watch="pause"
+        document.querySelector("#display").innerHTML = displayHours + ":" +displayMinutes +":"+ displaySeconds;
+        clearInterval();
+       }
+ }
+)
 
 reset.addEventListener("click",()=>{
 
